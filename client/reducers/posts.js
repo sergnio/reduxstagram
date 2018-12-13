b4a7060154482e27cf00409b5a056539e4900696
Sergnio <sergnio@gmail.com>
2018-12-13 13:18:04 -0600

    Added two base reducers, created combined reducer base

--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,15 +1,12 @@
-function posts(state = [], action) {
-  switch (action.type) {
-    case 'INCREMENT_LIKES' :
-      const i = action.index;
-      return [
-        ...state.slice(0, i),
-        {...state[i],  likes: state[i].likes + 1 },
-        ...state.slice(i + 1)
-      ];
-    default:
-      return state;
-  }
-}
+// A reducer takes in two things:
+// 1. the action (information about what happened)
+// 2. copy of current state
 
-export default posts;
+// input action and copy of store
+// do some updates
+// return new store
+
+export default function posts(state = [], action) {
+    console.log(state, action);
+    return state;
+}
