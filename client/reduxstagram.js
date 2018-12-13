e9676b71e7d072e29d891915d93204aa39ae7606
Sergnio <sergnio@gmail.com>
2018-12-13 13:23:31 -0600

    Added provider to main app to access our redux store

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -11,15 +11,20 @@ import PhotoGrid from './components/PhotoGrid';
 import css from './styles/style.styl';
 
 // import react router deps
-import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Router, Route, IndexRoute} from 'react-router';
+import {Provider} from 'react-redux';
+
+import store, {history} from './store';
 
 const router = (
-    <Router history={browserHistory}>
-        <Route path="/" component={Main}>
-            <IndexRoute component={PhotoGrid}/>
-            <Route path="/view/:postId" component={Single}/>
-        </Route>
-    </Router>
+    <Provider store={store}>
+        <Router history={history}>
+            <Route path="/" component={Main}>
+                <IndexRoute component={PhotoGrid}/>
+                <Route path="/view/:postId" component={Single}/>
+            </Route>
+        </Router>
+    </Provider>
 );
 
 render(router, document.getElementById('root'));
